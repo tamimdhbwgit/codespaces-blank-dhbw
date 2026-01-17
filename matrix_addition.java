fa046884257c9ad4f1fef21a3ea6470a33ce2c31
tamimdhbwgit <mdt.raihan.25@lehre.mosbach.dhbw.de>
2026-01-17 09:33:07 +0100

    Add files via upload

---
+++ matrix_addition.java
@@ -0,0 +1,31 @@
+public class matrix_addition{
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] B = {
+            {7, 8, 9},
+            {10, 11, 12}
+        };
+
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] C = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        
+        System.out.println("Sum of matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
---
+++ matrix_transpose.java
@@ -0,0 +1,26 @@
+public class matrix_transpose {
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] T = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                T[j][i] = A [i][j];
+            }
+        }
+        
+        System.out.println("Transpose of matrices:");
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[0].length; j++) {
+                System.out.print(T[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
