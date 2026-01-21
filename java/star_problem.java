7dc4c4b932e8008e9b0fd97b35354e6f3956e47a
tamimdhbwgit <mdt.raihan.25@lehre.mosbach.dhbw.de>
2026-01-21 08:23:34 +0100

    Add Loops to make a parrtern for Diamond

--- java/star_problem.java
+++ java/star_problem.java
@@ -25,5 +25,27 @@ public class star_problem{
             }
             System.out.println(" ");
         }
+        //to make a pattern like diamond
+        int n = 6;
+        for (int i = 0; i<=n; i++){
+           for (int j = i; j<6; j++){
+                System.out.print(" ");
+            }
+            for(int j =0; j<=i; j++){
+              System.out.print("* ");  
+            }
+            System.out.println();
+        }
+        for (int i = n; i>=0; i--){
+           for (int j = n; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int k =0; k<=i; k++){
+              System.out.print("* ");  
+            }
+            System.out.println();
+        }
+
+
     }
 }
